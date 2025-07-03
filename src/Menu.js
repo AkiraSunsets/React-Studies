b701a8e86e40b52bf0d2b877f9362073dddb5132
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-07-03 11:25:06 -0300

    Add files via upload

---
+++ src/Menu.js
@@ -0,0 +1,19 @@
+
+import './Menu.css';
+
+export default function Menu(props) {
+  return (
+    <div className="Menu">
+        <div className="header__logo">
+           <h2>Logo</h2>
+        </div>
+        <div className="header__menu">
+            <a href="">Home</a>
+            <a href="">Sobre</a>
+            <a href="">Produto</a>
+        </div>
+    </div>
+  );
+}
+
+
